d91791fcf89a07ae39b6683810e433986f6ca2ff
taojinxuan <823222209@example.com>
2024-08-03 01:10:13 +0800

    1

--- .whistle.cjs
+++ .whistle.cjs
@@ -4,7 +4,7 @@ const MY_DIST_PATH = path.resolve(__dirname, 'dist');
 exports.groupName = '专业班工程专题考试2024'; // 可选，设置分组， 要求 Whistle 版本 >= v2.9.21
 exports.name = `张巨芳`;
 exports.rules = `
-https://365.kdocs.cn/zhangjjufang1/dev/search/   http://localhost:5174
+https://365.kdocs.cn/zhangjjufang1/dev/search/   http://localhost:5173
 https://365.kdocs.cn/zhangjufang1/prod/search/   ${MY_DIST_PATH}\\
-wss://woa.wps.cn   http://localhost:5174
+wss://woa.wps.cn   http://localhost:5173
 `;
